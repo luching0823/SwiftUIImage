8edcd2a019bd9cadd1f8dac31486831c61d64a4c
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 13:21:01 +0800

    Initial Commit

---
+++ SwiftUIImage/SwiftUIImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageApp.swift
+//  SwiftUIImage
+//
+//  Created by 廖昱晴 on 2021/3/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
